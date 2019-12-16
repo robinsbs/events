187f9725367b7bcb01ebe684a68cad0cec7508c8
robin <robin@skybluesoftware.com>
2019-12-16 15:15:54 -0600

    creating sample app

---
+++ SkyBlueSoftware.Events.App/Body.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SkyBlueSoftware.Events.App
+{
+    public class Body
+    {
+        public Body(IEnumerable<ISubscribeTo> subscribers)
+        {
+            Subscribers = subscribers;
+        }
+
+        public IEnumerable<ISubscribeTo> Subscribers { get; }
+    }
+}
---
+++ SkyBlueSoftware.Events.App/Main.cs
@@ -0,0 +1,13 @@
+﻿namespace SkyBlueSoftware.Events.App
+{
+    public class Main
+    {
+        public Main(Body body)
+        {
+            Body = body;
+        }
+
+        public string Title => "Events Application";
+        public Body Body { get; }
+    }
+}
