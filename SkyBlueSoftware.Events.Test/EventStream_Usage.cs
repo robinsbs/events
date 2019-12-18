c69dbbb4e54182d6ff6e22a7a1746821f47bdde3
robin <robin@skybluesoftware.com>
2019-12-18 07:11:46 -0600

    building usage example

---
+++ SkyBlueSoftware.Events.Test/EventStream_Usage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SkyBlueSoftware.Events.Test
+{
+    [TestClass]
+    public class EventStream_Usage
+    {
+        [TestMethod]
+        public void EventStream_Usage_Example()
+        {
+            var events = new EventStream(new[] { new DetailViewModel() });
+            new ListViewModel(events).Select();
+        }
+    }
+
+    public class SelectedEvent { }
+
+    public class ListViewModel
+    {
+        private readonly IEventStream events;
+
+        public ListViewModel(IEventStream events)
+        {
+            this.events = events;
+        }
+
+        public void Select() => events.Publish(new SelectedEvent());
+    }
+
+    public class DetailViewModel : ISubscribeTo<SelectedEvent>
+    {
+        public async Task On(SelectedEvent e)
+        {
+            Console.WriteLine($"Received {e}");
+            await Task.CompletedTask;
+        }
+    }
+}
