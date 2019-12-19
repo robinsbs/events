31e7077211a793dcf7397e92467b630924035958
robin <robin@skybluesoftware.com>
2019-12-19 10:35:34 -0600

    adding dll to nuspec

--- SkyBlueSoftware.Events.Test.NuGet/EventStream_Tests_NuGet.cs
+++ SkyBlueSoftware.Events.Test.NuGet/EventStream_Tests_NuGet.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SkyBlueSoftware.TestFramework;
+using System.Threading.Tasks;
 
 namespace SkyBlueSoftware.Events.Test.NuGet
 {
     [TestClass]
-    public class EventStream_Tests
+    public class EventStream_Tests_NuGet
     {
         private TestHarness t = TestHarness.Create();
 
@@ -12,6 +13,11 @@ namespace SkyBlueSoftware.Events.Test.NuGet
         public void Initialize()
         {
             t = TestHarness.Create();
+        }
+
+        [TestMethod]
+        public async Task EventStream_Tests_NuGet_Test01()
+        {
         }
     }
 }
