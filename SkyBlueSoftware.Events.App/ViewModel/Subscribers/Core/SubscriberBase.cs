238b8d9132be8cf42113120ef9eb6686c8eef179
robin <robin@skybluesoftware.com>
2019-12-23 07:41:50 -0600

    added delay parameter for subscribers

--- SkyBlueSoftware.Events.App/ViewModel/Subscribers/Core/SubscriberBase.cs
+++ SkyBlueSoftware.Events.App/ViewModel/Subscribers/Core/SubscriberBase.cs
@@ -11,13 +11,16 @@ namespace SkyBlueSoftware.Events.ViewModel
         {
             Log = new ObservableCollection<string>();
             counter = 0;
+            Delay = "0";
         }
 
         public string Name => GetType().Name;
+        public string Delay { get; set; }
         public ObservableCollection<string> Log { get; }
 
         protected async Task LogEvent<T>(T e)
         {
+            if (int.TryParse(Delay, out var delay)) await Task.Delay(delay);
             Log.Insert(0, $"{++counter} - Received event {e?.GetType().Name}");
             await Task.CompletedTask;
         }
