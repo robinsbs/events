4cb50a59474d444ac658a99d0fcc093185d45e27
robin <robin@skybluesoftware.com>
2020-01-22 14:00:00 -0600

    storage tests

---
+++ SkyBlueSoftware.Storage.Test/Storage_Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SkyBlueSoftware.Storage.Test
+{
+    [TestClass]
+    public class Storage_Tests
+    {
+        [TestMethod]
+        public void Storage_Tests_Test01()
+        {
+        }
+    }
+}
