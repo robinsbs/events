e19213be44339902a0d2a8cca21cdf5ea4d500bf
robin <robin@skybluesoftware.com>
2020-01-23 10:22:05 -0600

    storage tests

--- SkyBlueSoftware.Storage.Test/Storage_Tests.cs
+++ SkyBlueSoftware.Storage.Test/Storage_Tests.cs
@@ -1,4 +1,7 @@
+using Microsoft.Data.SqlClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SkyBlueSoftware.Storage.Test
 {
@@ -6,8 +9,27 @@ namespace SkyBlueSoftware.Storage.Test
     public class Storage_Tests
     {
         [TestMethod]
-        public void Storage_Tests_Test01()
+        public async Task Storage_Tests_Test01()
         {
+            var source = new CancellationTokenSource();
+            var token = source.Token;
+            using (var connection = new SqlConnection("Server=(local);Integrated Security=true;"))
+            {
+                await connection.OpenAsync(token);
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "select * from sbs.dbo.document";
+                    var reader = await command.ExecuteReaderAsync(token);
+                    while (await reader.ReadAsync(token))
+                    {
+                    }
+                }
+            }
         }
+
+        //private IAsyncEnumerable<IRecord> ReadAsync()
+        //{
+
+        //}
     }
 }
