50b1b6dcad6c5f0e14040a7b67c05dee61f108c1
Robin Martin <robin@skybluesoftware.com>
2020-03-03 06:56:35 -0600

    added Framework project

---
+++ SkyBlueSoftware.Framework/LookupCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SkyBlueSoftware.Framework
+{
+    public class LookupCollection<TKey, TValue> where TKey : notnull where TValue : notnull
+    {
+        private readonly IDictionary<TKey, TValue> collection;
+        private readonly Func<TKey, TValue> defaultValue;
+
+        public LookupCollection(IDictionary<TKey, TValue> collection, Func<TKey, TValue> defaultValue)
+        {
+            this.collection = collection;
+            this.defaultValue = defaultValue;
+        }
+
+        public TValue this[TKey key] => collection.TryGetValue(key, out var value) ? value : defaultValue(key);
+        public void Add(TKey key, TValue value) => collection.Add(key, value);
+    }
+}
